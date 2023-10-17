0358683fb06ea742daa1d98e0938eb8431211d45
ChetanR01 <chetan0101career@gmail.com>
2023-10-18 00:16:47 +0530

    generate report done

--- App/views.py
+++ App/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Teacher,Student
-
+from .utils import render_to_pdf
+from datetime import datetime,date
 
 def index(request):
     return render(request,"index.html",{})
@@ -29,32 +30,28 @@ def student_teacher(request,operation):
         return redirect("/student-teacher/default")
 
     elif operation=="generate-certificate":
-        teacher_id = request.POST["teacher"]
-        student_id = request.POST["student"]
+        teacher_id = request.POST.get("teacher")
+        student_id = request.POST.get("student")
 
         if teacher_id=="None":
             return redirect("/student-teacher/default")
         if student_id=="None":
             return redirect("/student-teacher/default")
-
-        teacher = Teacher.objects.get(pk=teacher_id)
-        students_4_teacher= teacher.students.all()
-
-        student = Student.objects.get(pk=student_id)
-        teachers_4_student = student.teacher_set.all()
+        
+        teacher = Teacher.objects.get(id=teacher_id)
+        student = Student.objects.get(id=student_id)
 
         template_name = "certificate-template.html"
 
-        return render_to_pdf(template_name,{"user":user,
-                "rep_order":rep_order,
-                "order_id":order_id,
+        return render_to_pdf(template_name,{"teacher":teacher.name,
+                "student":student.name,
+                "date":date.today(),
             },
     )
     
     else:
         return render(request,"studentteacher.html",{"allstudents":allstudents,"allteachers":allteachers})
     
-    return render(request,"studentteacher.html",{"allstudents":allstudents,"allteachers":allteachers})
 
 
 def students_for_teacher(request, teacher_id):
